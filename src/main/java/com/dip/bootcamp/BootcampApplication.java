f895592134c43a467fe635da2196c52d1f665c6d
Nanra Sukedy <sukedynanra@gmail.com>
2020-08-03 15:35:40 +0700

    :repeat: Edit Main Application :repeat:

--- src/main/java/com/dip/bootcamp/BootcampApplication.java
+++ src/main/java/com/dip/bootcamp/BootcampApplication.java
@@ -11,20 +11,6 @@ public class BootcampApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootcampApplication.class, args);
-
-		// Instance Model Object
-		Employee dataEmployee = new Employee();
-		dataEmployee.setId(7);
-		dataEmployee.setName("Nanra Sukedy");
-		dataEmployee.setEmail("nanrasukedy@outlook.com");
-		dataEmployee.setAddress("Bandung");
-
-		// Print Out Data Employee
-		System.out.println("Employee Id: " + dataEmployee.getId());
-		System.out.println("Employee Name: " + dataEmployee.getName());
-		System.out.println("Employee Email: " + dataEmployee.getEmail());
-		System.out.println("Employee Address: " + dataEmployee.getAddress());
-
 	}
 
 }
