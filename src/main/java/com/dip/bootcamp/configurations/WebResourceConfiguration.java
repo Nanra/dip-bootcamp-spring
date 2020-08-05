605f67dc48766df380f19b42e8ade3492d53f954
Nanra Sukedy <sukedynanra@gmail.com>
2020-08-05 10:45:42 +0700

    :hammer: Add Web Resource Configuration Handler :hammer:

---
+++ src/main/java/com/dip/bootcamp/configurations/WebResourceConfiguration.java
@@ -0,0 +1,21 @@
+package com.dip.bootcamp.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebResourceConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+    }
+
+}
+
