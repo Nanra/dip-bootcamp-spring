042d2078fee43bfa515ebf6dd262dc13a51724b4
Nanra Sukedy <sukedynanra@gmail.com>
2020-08-07 13:31:44 +0700

    :repeat: Edit Login Controller and Login HTML :repeat:

--- src/main/java/com/dip/bootcamp/controllers/LoginController.java
+++ src/main/java/com/dip/bootcamp/controllers/LoginController.java
@@ -6,8 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.Date;
-
 @Controller
 public class LoginController {
 
