77e9c0c7138ba5c4cdd837c060ebb3fcf547c92f
Nanra Sukedy <sukedynanra@gmail.com>
2020-08-12 13:10:50 +0700

    :hammer: Add Dummy login page Logic :hammer:

--- src/main/java/com/dip/bootcamp/controllers/HomePageController.java
+++ src/main/java/com/dip/bootcamp/controllers/HomePageController.java
@@ -3,23 +3,31 @@ package com.dip.bootcamp.controllers;
 import com.dip.bootcamp.utilities.InformationConstant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Date;
 
 @Controller
+@RequestMapping(value = "/dashboard")
 public class HomePageController {
 
-    @RequestMapping(value = "/dashboard", method = {RequestMethod.GET, RequestMethod.POST})
+    @GetMapping(value = (""))
     public String dashboard(Model model){
         String pageTitle = "Dashboard" + InformationConstant.websiteTitle;
 
         model.addAttribute("username", "Nanra");
         model.addAttribute("title", pageTitle);
         return "dashboard";
+    }
+
+    @GetMapping(value = "/detail-visitor")
+    public ResponseEntity dashboardDetailVisitor(){
+        return ResponseEntity.ok("This is Page Detail Visitor");
     }
 
 }
--- src/main/java/com/dip/bootcamp/controllers/LoginController.java
+++ src/main/java/com/dip/bootcamp/controllers/LoginController.java
@@ -1,20 +1,41 @@
 package com.dip.bootcamp.controllers;
 
+import com.dip.bootcamp.models.Login;
 import com.dip.bootcamp.utilities.InformationConstant;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class LoginController {
 
     @RequestMapping(value = {"/", "/login"}, method = {RequestMethod.GET, RequestMethod.POST})
-    public String index(Model model){
+    public String loginPage(Model model, @ModelAttribute("dataLogin") Login dataLogin){
         String pageTitle = "Login" + InformationConstant.websiteTitle;
 
         model.addAttribute("title", pageTitle);
         return "login";
+    }
+
+    @RequestMapping(value = {"/validate-login"}, method = {RequestMethod.GET, RequestMethod.POST})
+    public RedirectView loginValidation(@ModelAttribute("dataLogin") Login dataLogin, Model model){
+
+        if (dataLogin.getUsername().equalsIgnoreCase("Nanra")
+                && dataLogin.getPassword().equalsIgnoreCase("password")) {
+
+            System.out.println("Authenticated");
+
+            return new RedirectView("/dashboard");
+        } else {
+            System.out.println("UnAuthenticated");
+            return new RedirectView("/login");
+        }
+
+
     }
 
 }
