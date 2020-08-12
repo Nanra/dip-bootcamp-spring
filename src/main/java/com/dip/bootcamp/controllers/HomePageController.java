4a97bc5052e979f85f480125d45ff219ea547d80
Nanra Sukedy <sukedynanra@gmail.com>
2020-08-12 10:52:06 +0700

    :hammer: Optimized Dashboard with Fragments :hammer:

--- src/main/java/com/dip/bootcamp/controllers/HomePageController.java
+++ src/main/java/com/dip/bootcamp/controllers/HomePageController.java
@@ -13,21 +13,6 @@ import java.util.Date;
 @Controller
 public class HomePageController {
 
-    private String appMode;
-
-    @Autowired
-    public HomePageController(Environment environment){
-        appMode = environment.getProperty("app-mode");
-    }
-
-//    @RequestMapping(value = "/", method = {RequestMethod.GET, RequestMethod.POST})
-//    public String index(Model model){
-//        model.addAttribute("datetime", new Date());
-//        model.addAttribute("username", "Nanra");
-////        model.addAttribute("mode", appMode);
-//        return "index";
-//    }
-
     @RequestMapping(value = "/dashboard", method = {RequestMethod.GET, RequestMethod.POST})
     public String dashboard(Model model){
         String pageTitle = "Dashboard" + InformationConstant.websiteTitle;
