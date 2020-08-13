ae8413e836abf2d9076fd431d427673c028091f9
Nanra Sukedy <sukedynanra@gmail.com>
2020-08-13 17:21:38 +0700

    :hammer: Add JS event Save new Employee :hammer:

---
+++ src/main/resources/static/jsmodules/employee/form-employee.js
@@ -0,0 +1,46 @@
+
+
+
+$(document).off("click", "#btn-save");
+$(document).on("click", "#btn-save", function(e) {
+
+    console.log("Masuk Button Save");
+
+    let dataBody = {
+        name: $("#form-employee #name").val(),
+        email: $("#form-employee #email").val(),
+        phone: $("#form-employee #phone").val(),
+        address: $("#form-employee #address").val(),
+    }
+
+    console.log(dataBody);
+
+    $.when(dipAjax.post('/api/employee/save', dataBody)).done(function(result) {
+        if (result.statusCode === "201") {
+            console.log(result);
+            swal({
+                title: "Save Success!",
+                text: "Your data has been saved!",
+                icon: "success",
+                timer: 3000,
+                button: false
+            }).then(() => {
+                window.location = "/employee/list";
+            });
+        } else {
+            console.log(result);
+            swal({
+                title: "Failed!",
+                text: "Failed to Save Data",
+                icon: "error",
+                button: "OK",
+                timer: 5000
+            });
+            return false;
+        }
+    });
+
+
+});
+
+
