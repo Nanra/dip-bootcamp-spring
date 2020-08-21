2723508edc2e4d7ff2c26ef60e24e3ae12daffc2
Nanra Sukedy <sukedynanra@gmail.com>
2020-08-21 14:23:15 +0700

    :hammer: Add Product Entity :hammer:

---
+++ src/main/java/com/dip/bootcamp/models/Product.java
@@ -0,0 +1,23 @@
+package com.dip.bootcamp.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    public String id;
+    public String name;
+    public String quantity;
+    public String sku;
+    public String createBy;
+    public Timestamp createDate;
+    public String editBy;
+    public Timestamp editDate;
+
+}
